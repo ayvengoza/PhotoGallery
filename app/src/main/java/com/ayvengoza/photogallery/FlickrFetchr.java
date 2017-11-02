69a76cdd834b384a64ec2151fc55cc3294b37157
Andrii Zastupailo <a.zastupailo@samsung.com>
2017-11-02 19:16:42 +0200

    add work with flickr api

--- app/src/main/java/com/ayvengoza/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/ayvengoza/photogallery/FlickrFetchr.java
@@ -1,5 +1,8 @@
 package com.ayvengoza.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,6 +14,10 @@ import java.net.URL;
  */
 
 public class FlickrFetchr {
+    private static final String TAG = "FlickFetch";
+    private static final String API_KEY = "b6d53e03aa72f61d87b4214eb900e8e5";
+    private static final String API_SECRET = "a1c24b42356cc3bf";
+
     public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -36,5 +43,23 @@ public class FlickrFetchr {
 
     public String getUrlString(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
+    }
+
+    public void fetchItems(){
+        try {
+            String url = Uri.parse("https://api.flickr.com/services/rest/")
+                    .buildUpon()
+                    .appendQueryParameter("method", "flickr.photos.getRecent")
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("format", "json")
+                    .appendQueryParameter("nojsoncallback", "1")
+                    .appendQueryParameter("extras", "url_s")
+                    .build().toString();
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to fetch items", e);
+        }
+
     }
 }
--- app/src/main/java/com/ayvengoza/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/ayvengoza/photogallery/PhotoGalleryFragment.java
@@ -1,19 +1,24 @@
 package com.ayvengoza.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.IOException;
 
 /**
  * Created by ang on 02.11.17.
  */
 
 public class PhotoGalleryFragment extends Fragment {
+    private static final String TAG = "PhotoGalleryFragment";
     private RecyclerView mPhotoRecyclerView;
 
     public static Fragment newInstance(){
@@ -27,6 +32,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemTask().execute();
     }
 
     @Nullable
@@ -36,5 +42,13 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView = (RecyclerView) view.findViewById(R.id.photo_resycler_view);
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
         return view;
+    }
+
+    private class FetchItemTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            new FlickrFetchr().fetchItems();
+            return null;
+        }
     }
 }
